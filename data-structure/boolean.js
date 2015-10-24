1ace367fea350ac90f556ae020d79bef1e0bd646
Murad Rogozhnikov <murad@rogozhnikoff.com>
2015-10-24 21:51:39 +0300

    structure + null+undefined

---
+++ data-structure/boolean.js
@@ -0,0 +1,18 @@
+/*
+
+  True - Да
+  False - Нет
+
+* */
+
+
+/*
+  Как их можно определить?
+
+  по типу:
+  typeof someVariable === 'boolean' // -> true || false
+
+  по родителькому обьекту:
+  someVariable instanceof Boolean
+
+* */
---
+++ operators/for-while.js
@@ -0,0 +1,19 @@
+/*
+
+ for ( ; ; ) {
+  continue
+  break
+ }
+
+
+ for ( in ) {
+   continue
+   break
+ }
+
+ while () {
+   continue
+   break
+ }
+
+ * */
